b11d596b309718b35d224a3f736dd691025515a9
lgLuis <luis_unlz@yahoo.com.ar>
2020-12-21 02:05:30 -0300

    funciona con errores, pero sin el back.

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,20 +8,20 @@ class Search extends Component {
             serie: '',
             image: '#',
             resumen: '',
-            link: ''
+
         }
     }
 
     myAppi = async () => {
-        //        let res = await fetch("http://api.tvmaze.com/search/shows?q=alf")
-        let res = await fetch(`http://api.tvmaze.com/search/shows?q=${this.state.serie}/`)
+ //       let res = await fetch("https://api.themoviedb.org/3/search/movie?api_key=b78674d3628205a6d6750bf73d941a40&query=Frozen")
+        let res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=b78674d3628205a6d6750bf73d941a40&query=${this.state.serie}/`)
         let data = await res.json()
-        console.log(data[0].show.name)
+ //       console.log(data[0].show.name)
         this.setState({
-            serie: data[0].show.name,
-            image: data[0].show.image.medium,
-            resumen: data[0].show.summary,
-            link: data[0].show.officialSite
+            serie: data.results[0].original_title,
+            image: data.results[0].poster_path,
+            resumen: data.results[0].overview,
+
         })
     }
 
@@ -42,25 +42,23 @@ class Search extends Component {
             <div>
                 <div className="card text-center">
                     <div className=" header-fondo">
-                        BUSCADOR DE SERIES 
+                        BUSCADOR DE PELICULAS
                     <i class="far fa-smile-wink"></i>
                     </div>
                     <div className="card-body ppal">
                         <form onSubmit={this.handleSubmit}>
-                            <label className="label-color" >INGRESE EL NOMBRE DE LA SERIE: </label>
+                            <label className="label-color" > PELICULA: </label>
                             <br />
                             <input className="input-color"
                                 type="text"
-                                placeholder="por ejemplo ALF"
+                                placeholder="por ejemplo ... "
                                 value={this.state.serie}
                                 onChange={this.handleName}
                             />
                             <br /> <br />
                             <button type="submit" className="btn btn-dark button-color">BUSCAR</button>
-                            <br/>
-                            <img src={this.state.image} alt={this.state.serie} title={this.state.serie}/>
-                            <br/>
-                            {(this.state.link!=="")&&<a href={this.state.link} target="_blank" className="sitio-color">SITIO WEB OFICIAL</a>}
+                            <br/>                            
+                            <img src={`https://image.tmdb.org/t/p/w300/`+this.state.image} />     
                             <br/>
                             <p>{this.state.resumen}</p>
                         </form>
@@ -77,86 +75,7 @@ class Search extends Component {
 }
 
 export default Search;
-
 
 /*
-import React, { Component } from 'react';
-import '../css/Search.css'
-
-class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            serie: '',
-            image: '#',
-            resumen: '',
-            link: ''
-        }
-    }
-
-    myAppi = async () => {
-        //        let res = await fetch("http://api.tvmaze.com/search/shows?q=alf")
-        let res = await fetch(`http://api.tvmaze.com/search/shows?q=${this.state.serie}/`)
-        let data = await res.json()
-        console.log(data[0].show.name)
-        this.setState({
-            serie: data[0].show.name,
-            image: data[0].show.image.medium,
-            resumen: data[0].show.summary,
-            link: data[0].show.officialSite
-        })
-    }
-
-    handleName = event => {
-        this.setState({
-            serie: event.target.value
-        })
-    }
-
-    handleSubmit = event =>{
-        this.myAppi()
-        event.preventDefault()
-    }
-
- 
-    render() {
-        return (
-            <div>
-                <div className="card text-center">
-                    <div className=" header-fondo">
-                        BUSCADOR DE SERIES 
-                    <i class="far fa-smile-wink"></i>
-                    </div>
-                    <div className="card-body ppal">
-                        <form onSubmit={this.handleSubmit}>
-                            <label className="label-color" >INGRESE EL NOMBRE DE LA SERIE: </label>
-                            <br />
-                            <input className="input-color"
-                                type="text"
-                                placeholder="por ejemplo ALF"
-                                value={this.state.serie}
-                                onChange={this.handleName}
-                            />
-                            <br /> <br />
-                            <button type="submit" className="btn btn-dark button-color">BUSCAR</button>
-                            <br/>
-                            <img src={this.state.image} alt={this.state.serie} title={this.state.serie}/>
-                            <br/>
-                            {(this.state.link!=="")&&<a href={this.state.link} target="_blank" className="sitio-color">SITIO WEB OFICIAL</a>}
-                            <br/>
-                            <p>{this.state.resumen}</p>
-                        </form>
-                    </div>
-                    <div className=" footer-fondo">
-                        Luis 2<i class="far fa-smile"></i>2<i class="far fa-smile"></i>
-                    </div>
-                </div>
-
-
-            </div>
-        );
-    }
-}
-
-export default Search;
+                            <img src={`https://image.tmdb.org/t/p/w300/`+this.state.image!==""&&`https://image.tmdb.org/t/p/w300/`+this.state.image} />    
 */
